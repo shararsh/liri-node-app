e99489a3e05cb63e5c7f1bd1c23ae9f4384041ca
Sharareh Shafiei <sharareh@Shararehs-MacBook-Pro.local>
2018-01-31 22:49:08 -0800

    revised

--- liri.js
+++ liri.js
@@ -2,120 +2,152 @@ var twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
-
 var keys = require('./keys.js');
-var twitterKeys = keys.twitterKeys;
+
+
+console.log("Choices: my-tweets , spotify-this-song , movie-this , or do-what-it-says");
+
 
 var command = process.argv[2];
 var commandArgument = process.argv[3];
 
-switch (command) {
 
-	case 'my-tweets':
-		myTweets();
-		break;
-
-	case 'spotify-this-song':
-		mySpotify(commandArgument);
-		break;
-
-	case 'movie-this':
-		movieThis(commandArgument);
-		break;
-
-	case 'do-what-it-says':
-		doWhatItSays();
-		break;
-
-	default:
-		console.log("Command not Valid. Please try again!");
+for (i = 4; i < process.argv.length; i++) {
+    commandArgument += '+' + process.argv[i];
 }
 
-function myTweets() {
+function commands() {
 
-	var client = new Twitter({
-		consumer_key: twitterKeys.consumer_key,
-		consumer_secret: twitterKeys.consumer_secret,
-		access_token_key: twitterKeys.access_token_key,
-		access_token_secret: twitterKeys.access_token_secret
-	});
+    switch (command) {
 
-	client.get('statuses/user_timeline', {count: 20, trim_user: false, exclude_replies: true, include_rts: false}, function(error, tweets, response) {
+        case 'my-tweets':
+            getTweets();
+            break;
 
-		if (!error) {
-			for (var i=0; i<tweets.length; i++) {
-				console.log(tweets[i].created_at);
-				console.log(tweets[i].text);
-				}
-			}
-	});
+        case 'spotify-this-song':
+            getSpotify();
+            break;
 
-}
+        case 'movie-this':
+            getMovie();
+            break;
 
-function mySpotify(receivedSong) {
+        case 'do-what-it-says':
+            doIt();
+            break;
 
-	var mySong = receivedSong ? receivedSong : 'The Sign Ace of Base';
-
-	spotify.search({ type: 'track', query: mySong }, function(err, data) {
-
-		if ( err ) {
-        	return console.log('Error occurred: ' + err);
-    	}
-			console.log('Artist Name: ' + data.tracks.items[0].artists[0].name);
-			console.log('Song Name: ' + data.tracks.items[0].name);
-			console.log('Preview Link: ' + data.tracks.items[0].preview_url);
-			console.log('Album Title: ' + data.tracks.items[0].album.name);
-	});
-}
-
-function movieThis(receivedMovie) {
+    }
+};
 
 
-	var myMovie = receivedMovie ? receivedMovie : 'Mr. Nobody';
+function getTweets() {
+    console.log("Twitter History Below");
 
-	Request("http://www.omdbapi.com/?t=" + myMovie + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
-												
-		if (!error && response.statusCode === 200) {
+    var client = new twitter({
+        consumer_key: 'XnoYtv6oNCY1IZ4SXg8xtOmQh',
+        consumer_secret: 'CMZUpzwbEfM4QoyJ4k8mea6OQTr0n8y68cTx0X5cB2zM1mN5Tb',
+        access_token_key: '923355480138772480-SD0jE00C6dgsnsBKoPJDew3BE8h0iYW',
+        access_token_secret: 'xFOdcX3GnXuJFdum2C8Dno58OvMZ3j6mtHAEJbFFAeaeo',
+    });
 
-    		console.log('Movie Title: ' + JSON.parse(body).Title);
-    		console.log('Release Year: ' + JSON.parse(body).Year);
-    		console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
-    		console.log('Production Country: ' + JSON.parse(body).Country);
-    		console.log('Language: ' + JSON.parse(body).Language);
-    		console.log('Plot: ' + JSON.parse(body).Plot);
-    		console.log('Actors/Actresses: ' + JSON.parse(body).Actors);
-  		}
+    var parameters = {
+        screen_name: 'shararshk',
+        count: 20
+    };
 
-	});
+    client.get('statuses/user_timeline', parameters, function(error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++) {
+                var returnedData = ('Number: ' + (i + 1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+                console.log(returnedData);
+                console.log("------------");
+            }
+        };
+    });
+};
 
-}
 
-function doWhatItSays() {
 
-	fs.readFile('random.txt', 'utf8', function(error, data) {
 
-	if (error) return console.log('Filesystem Read Error: ' + error);
+function getSpotify() {
+    console.log("Awesome-sauce Songs!");
 
-	var dataObject = data.split(',');
-	var myFunction = dataObject[0];
-	var myArgument = dataObject[1];
+    var Spotify = new spotify({
+        id: "0693b99f481d484e9d25cff71e450645",
+        secret: "0f416499fb624542800ebd4cc7ed8637",
+    });
 
-	switch (myFunction) {
-		case 'my-tweets':
-			myFunction = 'myTweets';
-			break;
-		case 'spotify-this-song':
-			myFunction = 'mySpotify';
-			break;
-		case 'movie-this':
-			myFunction = 'movieThis';
-			break;
-		default:
-			console.log('Unexpected error in doWhatItSays function');
-	}
+    var searchTrack;
+    if (commandArgument === undefined) {
+        searchTrack = "The Sign Ace of Base";
+    } else {
+        searchTrack = commandArgument;
+    }
 
-	eval(myFunction)(myArgument);
 
-	});
+    Spotify.search({ type: 'track', query: searchTrack }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
 
-}
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Preview Here: " + data.tracks.items[0].preview_url);
+        }
+
+    });
+
+};
+
+function getMovie() {
+    console.log("Your Movie Info Below");
+
+    var searchMovie;
+    if (commandArgument === undefined) {
+        searchMovie = "Mr. Nobody";
+    } else {
+        searchMovie = commandArgument;
+    };
+
+    var url = 'http://www.omdbapi.com/?t=' + searchMovie + '&y=&plot=short&apikey=40e9cece';
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Title: " + JSON.parse(body)["Title"]);
+            console.log("Year: " + JSON.parse(body)["Year"]);
+            console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+            console.log("Country: " + JSON.parse(body)["Country"]);
+            console.log("Language: " + JSON.parse(body)["Language"]);
+            console.log("Plot: " + JSON.parse(body)["Plot"]);
+            console.log("Actors: " + JSON.parse(body)["Actors"]);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
+            console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+        }
+    });
+};
+
+function doIt() {
+    console.log("Looking at random.txt now");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+
+
+            var dataArr = data.split(',');
+            command = dataArr[0];
+            commandArgument = dataArr[1];
+
+            for (i = 2; i < dataArr.length; i++) {
+                commandArgument = commandArgument + "+" + dataArr[i];
+            };
+
+            commands();
+
+        };
+
+    });
+
+};
+
+commands();
